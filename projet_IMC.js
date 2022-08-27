2b85b4a3ceb38fbd1bf7d5979d57482728ad8486
HakimAHB <79860059+HakimIbnAhmed@users.noreply.github.com>
2022-08-27 20:35:35 +0100

    Add files via upload

---
+++ projet_IMC.js
@@ -0,0 +1,59 @@
+const BMIData = [
+  { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
+  { name: "Bonne santé", color: "green", range: [18.5, 25] },
+  { name: "Surpoids", color: "lightcoral", range: [25, 30] },
+  { name: "Obésité modérée", color: "orange", range: [30, 35] },
+  { name: "Obésité sévère", color: "crimson", range: [35, 40] },
+  { name: "Obésité morbide", color: "purple", range: 40 },
+];
+
+// IMC = poids en kg / taille² en m
+const value = document.querySelector(".value");
+const result = document.querySelector(".result");
+const form = document.querySelector(".main-bloc");
+const submit = document.querySelector("button");
+submit.addEventListener("click", firstStep);
+
+function firstStep(e) {
+  e.preventDefault();
+
+  getInputs();
+}
+
+function getInputs() {
+  const height = document.querySelector("#height").value;
+  const weight = document.querySelector("#weight").value;
+  const BMI = weight / Math.pow(height / 100, 2).toFixed(1);
+
+  if (!height || !weight || height <= 0 || weight <= 0) {
+    value.textContent = "Erreur";
+    value.style.color = "#B22222";
+    result.textContent = "Veuillez remplir des valeurs correctes";
+  } else {
+    value.textContent = BMI.toFixed(1);
+  }
+
+  calculateBMI(BMI);
+}
+
+function calculateBMI(elt) {
+  if (elt < BMIData[0].range[1]) {
+    value.style.color = `${BMIData[0].color}`;
+    result.textContent = `${BMIData[0].name}`;
+  } else if (elt >= BMIData[0].range[1] && elt < BMIData[1].range[1]) {
+    value.style.color = `${BMIData[1].color}`;
+    result.textContent = `${BMIData[1].name}`;
+  } else if (elt >= BMIData[1].range[1] && elt < BMIData[2].range[1]) {
+    value.style.color = `${BMIData[2].color}`;
+    result.textContent = `${BMIData[2].name}`;
+  } else if (elt >= BMIData[2].range[1] && elt < BMIData[3].range[1]) {
+    value.style.color = `${BMIData[3].color}`;
+    result.textContent = `${BMIData[3].name}`;
+  } else if (elt >= BMIData[3].range[1] && elt < BMIData[4].range[1]) {
+    value.style.color = `${BMIData[4].color}`;
+    result.textContent = `${BMIData[4].name}`;
+  } else {
+    value.style.color = `${BMIData[5].color}`;
+    result.textContent = `${BMIData[5].name}`;
+  }
+}
