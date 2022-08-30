abe5c5166358df7033fb96cf0607c3602d3904e7
HakimAHB <79860059+HakimIbnAhmed@users.noreply.github.com>
2022-08-30 14:57:57 +0100

    Add files via upload

--- projet_IMC.js
+++ projet_IMC.js
@@ -9,7 +9,6 @@ const BMIData = [
 
 // IMC = poids en kg / taille² en m
 const value = document.querySelector(".value");
-const result = document.querySelector(".result");
 const form = document.querySelector(".main-bloc");
 const submit = document.querySelector("button");
 submit.addEventListener("click", firstStep);
@@ -23,37 +22,43 @@ function firstStep(e) {
 function getInputs() {
   const height = document.querySelector("#height").value;
   const weight = document.querySelector("#weight").value;
+  const result = document.querySelector(".result");
   const BMI = weight / Math.pow(height / 100, 2).toFixed(1);
 
-  if (!height || !weight || height <= 0 || weight <= 0) {
+  if (
+    !height ||
+    !weight ||
+    height <= 40 ||
+    weight <= 0 ||
+    height >= 300 ||
+    weight >= 300
+  ) {
     value.textContent = "Erreur";
     value.style.color = "#B22222";
     result.textContent = "Veuillez remplir des valeurs correctes";
   } else {
     value.textContent = BMI.toFixed(1);
+    calculateBMI(BMI);
   }
-
-  calculateBMI(BMI);
-}
-
-function calculateBMI(elt) {
-  if (elt < BMIData[0].range[1]) {
-    value.style.color = `${BMIData[0].color}`;
-    result.textContent = `${BMIData[0].name}`;
-  } else if (elt >= BMIData[0].range[1] && elt < BMIData[1].range[1]) {
-    value.style.color = `${BMIData[1].color}`;
-    result.textContent = `${BMIData[1].name}`;
-  } else if (elt >= BMIData[1].range[1] && elt < BMIData[2].range[1]) {
-    value.style.color = `${BMIData[2].color}`;
-    result.textContent = `${BMIData[2].name}`;
-  } else if (elt >= BMIData[2].range[1] && elt < BMIData[3].range[1]) {
-    value.style.color = `${BMIData[3].color}`;
-    result.textContent = `${BMIData[3].name}`;
-  } else if (elt >= BMIData[3].range[1] && elt < BMIData[4].range[1]) {
-    value.style.color = `${BMIData[4].color}`;
-    result.textContent = `${BMIData[4].name}`;
-  } else {
-    value.style.color = `${BMIData[5].color}`;
-    result.textContent = `${BMIData[5].name}`;
+  function calculateBMI(elt) {
+    if (elt < BMIData[0].range[1]) {
+      value.style.color = `${BMIData[0].color}`;
+      result.textContent = `${BMIData[0].name}`;
+    } else if (elt >= BMIData[0].range[1] && elt < BMIData[1].range[1]) {
+      value.style.color = `${BMIData[1].color}`;
+      result.textContent = `${BMIData[1].name}`;
+    } else if (elt >= BMIData[1].range[1] && elt < BMIData[2].range[1]) {
+      value.style.color = `${BMIData[2].color}`;
+      result.textContent = `${BMIData[2].name}`;
+    } else if (elt >= BMIData[2].range[1] && elt < BMIData[3].range[1]) {
+      value.style.color = `${BMIData[3].color}`;
+      result.textContent = `${BMIData[3].name}`;
+    } else if (elt >= BMIData[3].range[1] && elt < BMIData[4].range[1]) {
+      value.style.color = `${BMIData[4].color}`;
+      result.textContent = `${BMIData[4].name}`;
+    } else {
+      value.style.color = `${BMIData[5].color}`;
+      result.textContent = `${BMIData[5].name}`;
+    }
   }
 }
